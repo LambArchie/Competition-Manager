76b307619836af1170c9bd416e0dfc20766c1233
Archie Lamb <LambArchie@users.noreply.github.com>
2019-02-11 23:58:47 +0000

    Fixed some linting in models

--- app/models.py
+++ app/models.py
@@ -8,6 +8,7 @@ def load_user(id):
     return User.query.get(int(id))
 
 class User(UserMixin, db.Model):
+    """Controls the User SQL Table"""
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -15,15 +16,18 @@ class User(UserMixin, db.Model):
     review = db.relationship('Review', backref='author', lazy='dynamic')
 
     def __repr__(self):
-        return '<User {}>'.format(self.username)   
+        return '<User {}>'.format(self.username)
 
     def set_password(self, password):
+        """Returns the hash of the given password"""
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
-        return check_password_hash(self.password_hash, password) 
+        """Checks if the password matches"""
+        return check_password_hash(self.password_hash, password)
 
 class Review(db.Model):
+    """Controls the Review SQL table"""
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
