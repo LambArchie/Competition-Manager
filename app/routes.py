a0a767b406ff3943a95997c046a0201a108b9a64
Archie Lamb <LambArchie@users.noreply.github.com>
2019-02-12 11:43:45 +0000

    Added user pages & last seen

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,17 @@
+from datetime import datetime
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 from app import app, db
 from app.forms import LoginForm, RegistrationForm
 from app.models import User
+
+@app.before_request
+def before_request():
+    """Code which is run before every request"""
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
 
 @app.route('/')
 @app.route('/index')
@@ -50,3 +58,13 @@ def register():
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    reviews = [
+        {'author': user, 'body': 'Test reviews #1'},
+        {'author': user, 'body': 'Test reviews #2'}
+    ]
+    return render_template('user.html', user=user, reviews=reviews)
