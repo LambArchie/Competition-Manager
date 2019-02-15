a349c830434d603effb49c650db8daf39536a709
Archie Lamb <LambArchie@users.noreply.github.com>
2019-02-15 10:47:28 +0000

    Linting Fix

--- app/errors.py
+++ app/errors.py
@@ -3,9 +3,11 @@ from app import app, db
 
 @app.errorhandler(404)
 def not_found_error(error):
+    """404 Page Missing handling"""
     return render_template('errors/404.html'), 404
 
 @app.errorhandler(500)
 def internal_error(error):
+    """500 Server Error handling"""
     db.session.rollback()
     return render_template('errors/500.html'), 500
