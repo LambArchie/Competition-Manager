e2b40e15e6c4637b6d8c6c5f14a1d9f0ca0753c8
Archie Lamb <LambArchie@users.noreply.github.com>
2019-03-27 11:34:48 +0000

    Added validation checks to manual new admin file

--- new_admin_user.py
+++ new_admin_user.py
@@ -1,12 +1,27 @@
+"""
+Allows manually creating admins
+"""
+from getpass import getpass
 from app import db
 from app.models import User
 
 username = input("Enter Admin Username: ")
 email = input("Enter Admin Email: ")
-password = input("Enter Admin Password: ")
-user = User(username=username, email=email, admin=True)
-user.set_password(password)
-db.session.add(user)
-db.session.commit()
+print("Password will not be echoed")
+password = getpass("Enter Admin Password: ")
 
-print("Admin added successfully")
+print()
+# Check if user already exists
+checkUser = User.query.filter_by(username=username).first()
+checkEmail = User.query.filter_by(email=email).first()
+if checkUser is not None:
+    print("Username is already in use")
+elif checkEmail is not None:
+    print("Email is already in use")
+else:
+    user = User(username=username, email=email, admin=True)
+    user.set_password(password)
+    db.session.add(user)
+    db.session.commit()
+
+    print("Admin added successfully")
