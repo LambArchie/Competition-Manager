d18bfb2d6a9a99995c097b37f6f300f2602374c2
Archie Lamb <LambArchie@users.noreply.github.com>
2019-07-04 14:21:44 +0100

    Added API to retrieve user info

---
+++ app/api_v1/users.py
@@ -0,0 +1,18 @@
+"""
+Users API Routes
+"""
+from flask import jsonify
+from app.api_v1 import bp
+from app.api_v1.auth import token_auth
+from app.api_v1.errors import error_response
+from app.models import User
+
+@bp.route('/users/<int:user_id>', methods=['GET'])
+@token_auth.login_required
+def get_user(user_id):
+    """Returns user from id"""
+    user = User.query.filter_by(id=user_id).first()
+    if user is not None:
+        return jsonify(user.to_json())
+    else:
+        return error_response(404, "user id doesn't exist")
