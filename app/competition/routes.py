202c4ec17ae8d1ba0f17fb90ec2b6a9f5b2c7c20
Archie Lamb <LambArchie@users.noreply.github.com>
2019-07-11 12:50:59 +0100

    Reorganised competitions routes

--- app/competition/routes.py
+++ app/competition/routes.py
@@ -16,16 +16,8 @@ from app.competition.forms import CompetitionCreateForm, CategoryCreateForm, Rev
 @login_required
 def index():
     """Makes dynamic page listing all competitions"""
-    competitions = [competition.to_json() for competition in Competition.query.all()]
-    return render_template('competition/index.html', title="Competitions", competitions=competitions)
-
-@bp.route('/<int:comp_id>')
-@login_required
-def competition_overview(comp_id):
-    """Makes dynamic competition pages"""
-    competition = Competition.query.filter_by(id=comp_id).first_or_404()
-    categories = [category.to_json() for category in Category.query.filter_by(comp_id=comp_id)]
-    return render_template('competition/competition.html', title=competition.name, name=competition.name, body=competition.body, categories=categories, id=comp_id)
+    comps = [competition.to_json() for competition in Competition.query.all()]
+    return render_template('competition/index.html', title="Competitions", competitions=comps)
 
 @bp.route('/create', methods=['GET', 'POST'])
 @login_required
@@ -42,19 +34,14 @@ def competition_create():
         return redirect(url_for('competition.competition_overview', comp_id=competition.id))
     return render_template('competition/competitionCreate.html', title='Competition Create', form=form)
 
-@bp.route('/<int:comp_id>/<int:cat_id>')
+@bp.route('/<int:comp_id>')
 @login_required
-def category_overview(comp_id, cat_id):
-    """Makes dynamic categories pages"""
-    category = Category.query.filter_by(id=cat_id).filter_by(comp_id=comp_id).first_or_404()
-    review = Review.query.filter_by(comp_id=comp_id).all()
-    reviews_in_cat = []
-    for i in range(len(review)):
-        for j in range(len(review[i].categories)):
-            if review[i].categories[j].id == cat_id:
-                reviews_in_cat.append(review[i].to_json())
-
-    return render_template('competition/category.html', title=category.name, name=category.name, body=category.body, reviews=reviews_in_cat, comp_id=comp_id, cat_id=cat_id)
+def competition_overview(comp_id):
+    """Makes dynamic competition pages"""
+    competition = Competition.query.filter_by(id=comp_id).first_or_404()
+    categories = [category.to_json() for category in Category.query.filter_by(comp_id=comp_id)]
+    return render_template('competition/competition.html', title=competition.name, name=competition.name,
+                           body=competition.body, categories=categories, id=comp_id)
 
 @bp.route('/<int:comp_id>/create', methods=['GET', 'POST'])
 @login_required
@@ -71,6 +58,42 @@ def category_create(comp_id):
         flash('Category created successfully')
         return redirect(url_for('competition.category_overview', comp_id=comp_id, cat_id=category.id))
     return render_template('competition/categoryCreate.html', title='Category Create', form=form)
+
+@bp.route('/<int:comp_id>/<int:cat_id>')
+@login_required
+def category_overview(comp_id, cat_id):
+    """Makes dynamic categories pages"""
+    category = Category.query.filter_by(id=cat_id).filter_by(comp_id=comp_id).first_or_404()
+    reviews = Review.query.filter_by(comp_id=comp_id).all()
+    cat_reviews = []
+    for _, review in enumerate(reviews):
+        for j in range(len(review.categories)):
+            if review.categories[j].id == cat_id:
+                cat_reviews.append(review.to_json())
+
+    return render_template('competition/category.html', title=category.name, name=category.name,
+                           body=category.body, reviews=cat_reviews, comp_id=comp_id, cat_id=cat_id)
+
+@bp.route('/<int:comp_id>/<int:cat_id>/create', methods=['GET', 'POST'])
+@login_required
+def review_create(comp_id, cat_id):
+    """Create reviews"""
+    form = ReviewCreateForm()
+    if form.validate_on_submit():
+        review = Review(name=form.name.data,
+                        body=form.body.data,
+                        user_id=int(current_user.id),
+                        comp_id=int(comp_id)
+                        )
+        db.session.add(review)
+        category = Category.query.filter_by(id=cat_id).filter_by(comp_id=comp_id).first_or_404()
+        review.categories.append(category)
+        db.session.commit()
+
+        flash('Review created successfully')
+        return redirect(url_for('competition.review_overview',
+                                comp_id=comp_id, cat_id=cat_id, review_id=review.id))
+    return render_template('competition/reviewCreate.html', title='Review Create', form=form)
 
 @bp.route('/<int:comp_id>/<int:cat_id>/<int:review_id>')
 @login_required
@@ -90,23 +113,3 @@ def review_overview(comp_id, cat_id, review_id):
     timestamp = arrowGet(review.timestamp).humanize()
     return render_template('competition/review.html', title=review.name, name=review.name,
                            body=body, user=user, humanTime=timestamp, machineTime=review.timestamp)
-
-@bp.route('/<int:comp_id>/<int:cat_id>/create', methods=['GET', 'POST'])
-@login_required
-def review_create(comp_id, cat_id):
-    """Create reviews"""
-    form = ReviewCreateForm()
-    if form.validate_on_submit():
-        review = Review(name=form.name.data,
-                        body=form.body.data,
-                        user_id=int(current_user.id),
-                        comp_id=int(comp_id)
-                        )
-        db.session.add(review)
-        category = Category.query.filter_by(id=cat_id).filter_by(comp_id=comp_id).first_or_404()
-        review.categories.append(category)
-        db.session.commit()
-
-        flash('Review created successfully')
-        return redirect(url_for('competition.review_overview', comp_id=comp_id, cat_id=cat_id, review_id=review.id))
-    return render_template('competition/reviewCreate.html', title='Review Create', form=form)
