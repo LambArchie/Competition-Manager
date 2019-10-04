7e5ad01176c95d8d05d99f52ff6995949ec1b878
Archie Lamb <LambArchie@users.noreply.github.com>
2019-10-04 22:21:05 +0100

    Linting fix

--- app/competition/category.py
+++ app/competition/category.py
@@ -2,7 +2,7 @@
 Controls pages directly related to categories
 """
 from flask import render_template, flash, redirect, url_for, abort
-from flask_login import current_user,login_required
+from flask_login import current_user, login_required
 from app import db
 from app.database.models import Competition, Category, Review
 from app.competition import bp
