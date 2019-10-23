f7c0626802ff5da6c82fda896f09dcdcace35fef
Archie Lamb <LambArchie@users.noreply.github.com>
2019-10-23 14:54:19 +0100

    Moved user profile permission check to function

--- app/user/routes.py
+++ app/user/routes.py
@@ -12,6 +12,11 @@ from app.database.models import User, Submission
 from app.user import bp
 from app.user.forms import EditProfileForm, UploadAvatarForm
 
+def check_permissions(username):
+    """Checks if can edit users"""
+    if not current_user.username == username:
+        abort(403)
+
 @bp.route('/<username>/')
 @login_required
 def user_profile(username):
@@ -35,41 +40,39 @@ def avatar(username):
 @login_required
 def upload_avatar(username):
     """Controls avatar uploads"""
-    if username == current_user.username:
-        form = UploadAvatarForm()
-        if request.method == 'POST' and 'avatar' in request.files:
-            if form.validate_on_submit():
-                user = User.query.filter_by(username=current_user.username).first()
-                if (user.avatar != "") and (path.exists(
-                        current_app.config['UPLOADS_DEFAULT_DEST']+"avatars/"+user.avatar)):
-                    remove(current_app.config['UPLOADS_DEFAULT_DEST'] + "avatars/" + user.avatar)
-                    user.avatar_filename("")
-                file_obj = request.files['avatar']
-                file_extension = (file_obj.filename.split('.')[-1]).lower()
-                file_name = secure_filename(current_user.username + '.' + file_extension)
-                file_name = avatar_uploads.save(file_obj, name=file_name)
-                user.avatar_filename(file_name)
-                db.session.commit()
-                flash('Avatar updated')
-                return redirect(url_for('user.user_profile', username=current_user.username))
-            flash('Avatar not uploaded')
-        return render_template('users/upload_avatar.html', title='Upload Avatar', form=form)
-    abort(403)
+    check_permissions(username)
+    form = UploadAvatarForm()
+    if request.method == 'POST' and 'avatar' in request.files:
+        if form.validate_on_submit():
+            user = User.query.filter_by(username=current_user.username).first()
+            if (user.avatar != "") and (path.exists(
+                    current_app.config['UPLOADS_DEFAULT_DEST']+"avatars/"+user.avatar)):
+                remove(current_app.config['UPLOADS_DEFAULT_DEST'] + "avatars/" + user.avatar)
+                user.avatar_filename("")
+            file_obj = request.files['avatar']
+            file_extension = (file_obj.filename.split('.')[-1]).lower()
+            file_name = secure_filename(current_user.username + '.' + file_extension)
+            file_name = avatar_uploads.save(file_obj, name=file_name)
+            user.avatar_filename(file_name)
+            db.session.commit()
+            flash('Avatar updated')
+            return redirect(url_for('user.user_profile', username=current_user.username))
+        flash('Avatar not uploaded')
+    return render_template('users/upload_avatar.html', title='Upload Avatar', form=form)
 
 @bp.route('/<username>/edit_profile', methods=['GET', 'POST'])
 @login_required
 def edit_profile(username):
     """Profile edit page"""
-    if username == current_user.username:
-        form = EditProfileForm(current_user.username, current_user.email)
-        if form.validate_on_submit():
-            current_user.username = form.username.data
-            current_user.email = form.email.data
-            db.session.commit()
-            flash('Your changes have been saved.')
-            return redirect(url_for('user.edit_profile', username=current_user.username))
-        elif request.method == 'GET':
-            form.username.data = current_user.username
-            form.email.data = current_user.email
-        return render_template('users/edit_profile.html', title='Edit Profile', form=form)
-    abort(403)
+    check_permissions(username)
+    form = EditProfileForm(current_user.username, current_user.email)
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('user.edit_profile', username=current_user.username))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    return render_template('users/edit_profile.html', title='Edit Profile', form=form)
